901a777c4843c8023286107aa4872d4e36c69241
Sébastien Quirrion <sebastien@fitlab.fr>
2021-03-03 18:38:24 +0100

    WIP : modifs page login et ChoosePassword suite retour UI

--- src/components/anonymous/Login.js
+++ src/components/anonymous/Login.js
@@ -46,7 +46,7 @@ function Login() {
           <div className="rf-col-1"></div>
           {/* Title */}
           <div className="rf-col-10" style={{ textAlign: 'center' }}>
-            <h1 style={{ margin: '0' }}>Bienvenue sur le Portail<br/>de la communauté des conseillers<br/>numériques France Service</h1>
+            <h1 className="titrage" style={{ margin: '0' }}>Bienvenue sur le Portail<br/>de la communauté des conseillers<br/>numériques France Service</h1>
           </div>
           <div className="rf-col-1"></div>
         </div>
@@ -54,18 +54,18 @@ function Login() {
           <div className="rf-col-1"></div>
           {/* Avatars */}
           <div className="rf-col-10" style={{ textAlign: 'center' }}>
-            <img src="/avatars/avatar-conseiller.svg" width="75px" alt="Avatar conseiller" className="rf-mr-4w"/>
-            <img src="/avatars/avatar-conseillere.svg" width="75px" alt="Avatar conseillere" className="rf-mr-4w"/>
-            <img src="/avatars/avatar-conseillers.svg" width="75px" alt="Avatar conseillers" className="rf-mr-4w"/>
-            <img src="/avatars/avatar-senior.svg" width="75px" alt="Avatar senior" className="rf-mr-4w"/>
-            <img src="/avatars/avatar-coordinatrice.svg" width="75px" alt="Avatar coordinatrice"/>
+            <img src="/avatars/avatar-conseiller.svg" width="125px" alt="Avatar conseiller" className="rf-ml-4w rf-mr-4w"/>
+            <img src="/avatars/avatar-conseillere.svg" width="125px" alt="Avatar conseillere" className="rf-mr-4w"/>
+            <img src="/avatars/avatar-conseillers.svg" width="125px" alt="Avatar conseillers" className="rf-mr-4w"/>
+            <img src="/avatars/avatar-senior.svg" width="125px" alt="Avatar senior" className="rf-mr-4w"/>
+            <img src="/avatars/avatar-coordinatrice.svg" width="125px" alt="Avatar coordinatrice"/>
           </div>
           <div className="rf-col-1"></div>
         </div>
         <div className="rf-grid-row rf-grid-row--top rf-grid-row--center rf-pb-7w">
-          <div className="rf-col-5"></div>
+          <div className="rf-col-sm-1 rf-col-lg-4"></div>
           {/* Form */}
-          <div className="rf-col-2" style={{ textAlign: 'center' }}>
+          <div className="rf-col-sm-8 rf-col-lg-4" style={{ textAlign: 'center' }}>
             <div>
               {error && <span className="invalid">`{error.error}`</span>}
             </div>
@@ -93,7 +93,7 @@ function Login() {
               <br/>{loggingIn && <span>Connexion en cours...</span>}
             </div>
           </div>
-          <div className="rf-col-5"></div>
+          <div className="rf-col-sm-1 rf-col-lg-4"></div>
         </div>
       </div>
       {/* End content */}
