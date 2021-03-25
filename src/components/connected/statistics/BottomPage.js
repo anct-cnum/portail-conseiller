777d69e2c6f027ef5e10966997a1b88eeed2658c
morgan <morgan@fitlab.fr>
2021-03-25 17:27:12 +0100

    mise en place des différents Charts

--- src/components/connected/statistics/BottomPage.js
+++ src/components/connected/statistics/BottomPage.js
@@ -1,6 +1,141 @@
 import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 function BottomPage() {
+  const optionsChart1 = {
+    chart: {
+      zoomType: 'xy'
+    },
+    title: {
+      text: 'Evolution des accompagnements'
+    },
+    xAxis: [{
+      categories: ['Avril', 'Mai', 'Juin', 'Juillet'],
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      title: {
+        text: '',
+      }
+    }, { // Secondary yAxis
+      title: {
+        text: '',
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
+      },
+      opposite: true
+    }],
+    tooltip: {
+      shared: true
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 100,
+      verticalAlign: 'top',
+      y: 300,
+      floating: true,
+      backgroundColor:
+          Highcharts.defaultOptions.legend.backgroundColor || // theme
+          'rgba(255,255,255,0.25)'
+    },
+    series: [{
+      name: 'Accompagnement cumulés',
+      type: 'column',
+      yAxis: 1,
+      data: [25, 85, 45, 75],
+    }, {
+      name: 'Accompagnement par mois',
+      type: 'spline',
+      data: [25, 110, 155, 230],
+    }]
+  };
+
+  const optionsChart2 = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Tranches d\'âge des usagers'
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal'
+      }
+    },
+    series: [{
+      name: 'Plus de 60 ans',
+      data: [5],
+      tooltip: {
+        valueSuffix: '%'
+      }
+    }, {
+      name: '30-60 ans',
+      data: [2],
+      tooltip: {
+        valueSuffix: '%'
+      }
+    }, {
+      name: '18-35 ans',
+      data: [3],
+      tooltip: {
+        valueSuffix: '%'
+      }
+    }, {
+      name: '12-18 ans',
+      data: [4],
+      tooltip: {
+        valueSuffix: '%'
+      }
+    }, {
+      name: '- 12 ans',
+      data: [2],
+      tooltip: {
+        valueSuffix: '%'
+      }
+    }]
+  };
+
+  const optionsChart3 = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Statut des usagers'
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal'
+      }
+    },
+    series: [{
+      name: 'Retraité',
+      data: [37]
+    }, {
+      name: 'Sans emploi',
+      data: [35]
+    }, {
+      name: 'En emploi',
+      data: [20]
+    }, {
+      name: 'Etudiant',
+      data: [8]
+    }, {
+      name: 'Non renseigné',
+      data: [2]
+    }]
+  };
 
   return (
     <div className="rf-container">
@@ -8,9 +143,9 @@ function BottomPage() {
         <div className="rf-col-4"><hr></hr></div>
         <div className="rf-col-4"><hr></hr></div>
         <div className="rf-col-4"><hr></hr></div>
-        <div className="rf-col-4">Graph 1 Bas</div>
-        <div className="rf-col-4">Graph 2 Bas</div>
-        <div className="rf-col-4">Graph 3 Bas</div>
+        <div className="rf-col-4"><HighchartsReact highcharts={Highcharts} options={optionsChart1} /></div>
+        <div className="rf-col-4"><HighchartsReact highcharts={Highcharts} options={optionsChart2} /></div>
+        <div className="rf-col-4"><HighchartsReact highcharts={Highcharts} options={optionsChart3} /></div>
         <div className="rf-col-12"><hr></hr></div>
       </div>
     </div>
--- src/components/connected/statistics/RightPage.js
+++ src/components/connected/statistics/RightPage.js
@@ -1,15 +1,87 @@
 import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 function RightPage() {
+  const themesAccompagnement = [
+    'Équipement informatique 5',
+    'Naviguer sur Internet 40',
+    'Courriels 10',
+    'Applications smartphone 7',
+    'Gestion de contenus numériques 6',
+    'Env., vocab. numérique 21',
+    'Traitement de texte 8',
+    'Échanger avec ses proches 13',
+    'Emploi, formation 18',
+    'Accompagner son enfant 9',
+    'Numérique et TPE/PME 25',
+    'Démarche en ligne 17'
+  ];
+
+  const optionsChart1 = {
+
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Thèmes des accompagnements'
+    },
+    xAxis: {
+      categories: themesAccompagnement,
+      title: {
+        text: null
+      }
+    },
+    series: [{
+      data: [5, 40, 10, 7, 6, 21, 8, 13, 18, 9, 25, 17]
+    }]
+  };
+
+  const optionsChart2 = {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: true
+      }
+    },
+    title: {
+      text: 'Lieux des accompagnements'
+    },
+    series: [{
+      data: [1, 2, 3]
+    }]
+  };
+
+  const optionsChart3 = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Durée des accompagnements'
+    },
+    series: [{
+      data: [1, 3, 2, 5]
+    }]
+  };
 
   return (
     <div className="rf-container">
       <div className="rf-grid-row">
         <div className="rf-col-12"><hr></hr></div>
-        <div className="rf-col-12">Graph 1 Droite</div>
+        <div className="rf-col-12"><HighchartsReact highcharts={Highcharts} options={optionsChart1} /></div>
         <div className="rf-col-12"><hr></hr></div>
-        <div className="rf-col-6">Graph 2 Droite</div>
-        <div className="rf-col-6">Graph 3 Droite</div>
+        <div className="rf-col-6"><HighchartsReact highcharts={Highcharts} options={optionsChart2} /></div>
+        <div className="rf-col-6"><HighchartsReact highcharts={Highcharts} options={optionsChart3} /></div>
         <div className="rf-col-6"><hr></hr></div>
         <div className="rf-col-6"><hr></hr></div>
 
