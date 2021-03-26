72b6900cec56c223b10db986056b22e94adf9d18
morgan <morgan@fitlab.fr>
2021-03-26 16:49:43 +0100

    wip sur le style des graphiques

--- src/components/connected/statistics/BottomPage.js
+++ src/components/connected/statistics/BottomPage.js
@@ -1,139 +1,234 @@
 import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import roundedCorner from 'highcharts-rounded-corners';
 
 function BottomPage() {
+  roundedCorner(Highcharts);
+
   const optionsChart1 = {
+    credits: {
+      enabled: false
+    },
     chart: {
-      zoomType: 'xy'
+      zoomType: 'xy',
+      backgroundColor: '#1e1e1e',
     },
     title: {
-      text: 'Evolution des accompagnements'
+      text: 'Evolution des accompagnements',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
     },
     xAxis: [{
       categories: ['Avril', 'Mai', 'Juin', 'Juillet'],
-      crosshair: true
+      crosshair: true,
+      lineWidth: 0,
+      labels: {
+        style: {
+          color: '#cdc8c3',
+          fontSize: '15px'
+        }
+      }
     }],
     yAxis: [{ // Primary yAxis
+      tickWidth: 1,
+      tickColor: '#f7a35c',
+      gridLineWidth: 0,
       labels: {
         format: '{value}',
         style: {
-          color: Highcharts.getOptions().colors[1]
+          color: '#f7a35c',
         }
       },
       title: {
         text: '',
-      }
-    }, { // Secondary yAxis
-      title: {
-        text: '',
-      },
-      labels: {
-        format: '{value}',
-        style: {
-          color: Highcharts.getOptions().colors[0]
-        }
       },
       opposite: true
+    }, { // Secondary yAxis
+      tickWidth: 1,
+      tickColor: '#169b62',
+      gridLineWidth: 0,
+      title: {
+        text: '',
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          color: '#169b62'
+        }
+      },
     }],
+    plotOptions: {
+      column: {
+        borderWidth: 0
+      }
+    },
     tooltip: {
       shared: true
     },
     legend: {
-      layout: 'vertical',
-      align: 'left',
-      x: 100,
-      verticalAlign: 'top',
-      y: 300,
-      floating: true,
-      backgroundColor:
-          Highcharts.defaultOptions.legend.backgroundColor || // theme
-          'rgba(255,255,255,0.25)'
+      backgroundColor: '#1e1e1e',
+      itemStyle: {
+        color: '#cdc8c3',
+      }
     },
     series: [{
       name: 'Accompagnement cumulés',
       type: 'column',
       yAxis: 1,
       data: [25, 85, 45, 75],
+      color: '#169b62'
     }, {
       name: 'Accompagnement par mois',
-      type: 'spline',
       data: [25, 110, 155, 230],
+      lineWidth: 5,
+      color: '#f7a35c'
     }]
   };
 
   const optionsChart2 = {
+    credits: {
+      enabled: false
+    },
     chart: {
-      type: 'bar'
+      type: 'bar',
+      backgroundColor: '#1e1e1e',
+    },
+    yAxis: {
+      visible: false
+    },
+    xAxis: {
+      visible: false
     },
     title: {
-      text: 'Tranches d\'âge des usagers'
+      text: 'Tranches d\'âge des usagers',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
     },
     plotOptions: {
       series: {
-        stacking: 'normal'
+        stacking: 'normal',
+      },
+      bar: {
+        pointPadding: 0.4,
+        borderWidth: 0
+      }
+    },
+    legend: {
+      backgroundColor: '#1e1e1e',
+      itemStyle: {
+        color: '#cdc8c3',
       }
     },
     series: [{
-      name: 'Plus de 60 ans',
-      data: [5],
-      tooltip: {
-        valueSuffix: '%'
-      }
-    }, {
-      name: '30-60 ans',
-      data: [2],
-      tooltip: {
-        valueSuffix: '%'
-      }
-    }, {
-      name: '18-35 ans',
-      data: [3],
-      tooltip: {
-        valueSuffix: '%'
-      }
-    }, {
-      name: '12-18 ans',
+      name: '- 12 ans',
       data: [4],
       tooltip: {
         valueSuffix: '%'
-      }
+      },
+      color: '#ff0185',
+      borderRadiusTopLeft: '50%',
+      borderRadiusTopRight: '50%',
     }, {
-      name: '- 12 ans',
-      data: [2],
+      name: '12-18 ans',
+      data: [8],
       tooltip: {
         valueSuffix: '%'
-      }
+      },
+      color: '#5770be'
+    }, {
+      name: '18-35 ans',
+      data: [12],
+      tooltip: {
+        valueSuffix: '%'
+      },
+      color: '#cac5b0'
+    }, {
+      name: '35-60 ans',
+      data: [31],
+      tooltip: {
+        valueSuffix: '%'
+      },
+      color: '#ff6f4c'
+    }, {
+      name: 'Plus de 60 ans',
+      data: [45],
+      tooltip: {
+        valueSuffix: '%'
+      },
+      color: '#169b62',
+      borderRadiusBottomLeft: '50%',
+      borderRadiusBottomRight: '50%',
     }]
   };
 
   const optionsChart3 = {
+    credits: {
+      enabled: false
+    },
     chart: {
-      type: 'bar'
+      type: 'bar',
+      backgroundColor: '#1e1e1e',
+    },
+    yAxis: {
+      visible: false
+    },
+    xAxis: {
+      visible: false
     },
     title: {
-      text: 'Statut des usagers'
+      text: 'Statut des usagers',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
     },
     plotOptions: {
       series: {
-        stacking: 'normal'
+        stacking: 'normal',
+      },
+      bar: {
+        pointPadding: 0.4,
+        borderWidth: 0
+      }
+    },
+    legend: {
+      backgroundColor: '#1e1e1e',
+      itemStyle: {
+        color: '#cdc8c3',
       }
     },
     series: [{
-      name: 'Retraité',
-      data: [37]
-    }, {
-      name: 'Sans emploi',
-      data: [35]
-    }, {
-      name: 'En emploi',
-      data: [20]
+      name: 'Non renseigné',
+      data: [2],
+      color: '#a2b4b1',
+      borderRadiusTopLeft: '50%',
+      borderRadiusTopRight: '50%',
     }, {
       name: 'Etudiant',
-      data: [8]
+      data: [8],
+      color: '#ffdbd2'
     }, {
-      name: 'Non renseigné',
-      data: [2]
+      name: 'En emploi',
+      data: [20],
+      color: '#a3a6bc'
+    }, {
+      name: 'Sans emploi',
+      data: [35],
+      color: '#ddb094'
+    }, {
+      name: 'Retraité',
+      data: [37],
+      color: '#fff480',
+      borderRadiusBottomLeft: '50%',
+      borderRadiusBottomRight: '50%',
     }]
   };
 
--- src/components/connected/statistics/RightPage.js
+++ src/components/connected/statistics/RightPage.js
@@ -1,48 +1,126 @@
 import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import roundedCorner from 'highcharts-rounded-corners';
 
 function RightPage() {
+  roundedCorner(Highcharts);
+
   const themesAccompagnement = [
-    'Équipement informatique 5',
-    'Naviguer sur Internet 40',
-    'Courriels 10',
-    'Applications smartphone 7',
-    'Gestion de contenus numériques 6',
-    'Env., vocab. numérique 21',
-    'Traitement de texte 8',
-    'Échanger avec ses proches 13',
-    'Emploi, formation 18',
-    'Accompagner son enfant 9',
-    'Numérique et TPE/PME 25',
-    'Démarche en ligne 17'
+    'Équipement informatique &nbsp;&nbsp;<b>5</b>',
+    'Naviguer sur Internet &nbsp;&nbsp;<b>40</b>',
+    'Courriels &nbsp;&nbsp;<b>10</b>',
+    'Applications smartphone &nbsp;&nbsp;<b>7</b>',
+    'Gestion de contenus numériques &nbsp;&nbsp;<b>6</b>',
+    'Env., vocab. numérique &nbsp;&nbsp;<b>21</b>',
+    'Traitement de texte &nbsp;&nbsp;<b>8</b>',
+    'Échanger avec ses proches &nbsp;&nbsp;<b>13</b>',
+    'Emploi, formation &nbsp;&nbsp;<b>18</b>',
+    'Accompagner son enfant &nbsp;&nbsp;<b>9</b>',
+    'Numérique et TPE/PME &nbsp;&nbsp;<b>25</b>',
+    'Démarche en ligne &nbsp;&nbsp;<b>17</b>'
   ];
 
   const optionsChart1 = {
-
+    credits: {
+      enabled: false
+    },
     chart: {
-      type: 'bar'
+      type: 'bar',
+      backgroundColor: '#1e1e1e',
     },
     title: {
-      text: 'Thèmes des accompagnements'
+      text: 'Thèmes des accompagnements',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    tooltip: {
+      enabled: false
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 6,
+        borderWidth: 0
+      }
     },
     xAxis: {
       categories: themesAccompagnement,
       title: {
         text: null
-      }
+      },
+      lineWidth: 0,
+      gridLineWidth: 0,
+      labels: {
+        format: '{value}',
+        style: {
+          fontSize: 16,
+          color: '#ffffff',
+        }
+      },
+    },
+    yAxis: {
+      title: {
+        text: null
+      },
+      gridLineWidth: 0,
+      labels: '',
     },
     series: [{
-      data: [5, 40, 10, 7, 6, 21, 8, 13, 18, 9, 25, 17]
+      data: [
+        {
+          y: 5,
+          color: '#cac5b0',
+
+        }, {
+          y: 40,
+          color: '#abb8df',
+        }, {
+          y: 10,
+          color: '#fdcf41',
+        }, {
+          y: 7,
+          color: '#169b62',
+        }, {
+          y: 6,
+          color: '#80d5c6',
+        }, {
+          y: 21,
+          color: '#ff8d7e',
+        }, {
+          y: 8,
+          color: '#714753',
+        }, {
+          y: 13,
+          color: '#956052',
+        }, {
+          y: 18,
+          color: '#ddb094',
+        }, {
+          y: 9,
+          color: '#5770be',
+        }, {
+          y: 25,
+          color: '#ffed33',
+        }, {
+          y: 17,
+          color: '#be9b31',
+        }],
     }]
   };
 
   const optionsChart2 = {
+    credits: {
+      enabled: false
+    },
     chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
+      type: 'pie',
+      backgroundColor: '#1e1e1e',
     },
     plotOptions: {
       pie: {
@@ -51,26 +129,117 @@ function RightPage() {
         dataLabels: {
           enabled: false
         },
-        showInLegend: true
+        showInLegend: true,
+        borderWidth: 0
       }
     },
     title: {
-      text: 'Lieux des accompagnements'
+      text: 'Lieux des accompagnements',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
+    },
+    legend: {
+      itemStyle: {
+        color: '#cdc8c3',
+      }
     },
     series: [{
-      data: [1, 2, 3]
+      data: [{
+        name: 'À domicile',
+        y: 12,
+        color: '#ffcc9f'
+      }, {
+        name: 'À distance',
+        y: 15,
+        color: '#ff8d7e'
+      }, {
+        name: 'Autre lieu',
+        y: 27,
+        color: '#466964'
+      }, {
+        name: 'Lieu de rattachement',
+        y: 22,
+        color: '#5770be'
+      }]
     }]
   };
 
   const optionsChart3 = {
+    credits: {
+      enabled: false
+    },
     chart: {
-      type: 'column'
+      type: 'column',
+      backgroundColor: '#1e1e1e',
+    },
+    xAxis: {
+      categories: ['0 à 30 min.', '30 min. à 1h', '1h à 2h', '2h ou plus'],
+      title: {
+        text: null
+      },
+      gridLineWidth: 0,
+      lineWidth: 0,
+      className: 'highcharts-axis-dark',
+      labels: {
+        format: '<b>{value}</b>',
+        style: {
+          fontSize: 16,
+          color: '#cdc8c3',
+        }
+      },
+    },
+    yAxis: {
+      title: {
+        text: null
+      },
+      tickWidth: 1,
+      tickColor: '#ffffff',
+      gridLineWidth: 0,
+      className: 'highcharts-axis-dark',
+      labels: {
+        format: '<b>{value}</b>',
+        style: {
+          fontSize: 16,
+          color: '#cdc8c3',
+        }
+      },
+    },
+    plotOptions: {
+      column: {
+        borderWidth: 0
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    tooltip: {
+      pointFormat: '<div style="text-align: right">{point.y}</div>'
     },
     title: {
-      text: 'Durée des accompagnements'
+      text: 'Durée des accompagnements',
+      style: {
+        color: '#ffffff',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }
     },
     series: [{
-      data: [1, 3, 2, 5]
+      data: [{
+        y: 1,
+        color: '#abcdf5'
+      }, {
+        y: 3,
+        color: '#abcdf5'
+      }, {
+        y: 2,
+        color: '#abcdf5'
+      }, {
+        y: 5,
+        color: '#abcdf5'
+      }]
     }]
   };
 
@@ -82,9 +251,6 @@ function RightPage() {
         <div className="rf-col-12"><hr></hr></div>
         <div className="rf-col-6"><HighchartsReact highcharts={Highcharts} options={optionsChart2} /></div>
         <div className="rf-col-6"><HighchartsReact highcharts={Highcharts} options={optionsChart3} /></div>
-        <div className="rf-col-6"><hr></hr></div>
-        <div className="rf-col-6"><hr></hr></div>
-
       </div>
     </div>
   );
