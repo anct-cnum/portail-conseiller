b9bb214e008cf2da09b4418f35e96ccc26676a3d
morgan <morgan@fitlab.fr>
2021-08-09 12:01:47 +0200

    successResetPassword dans le reducer

--- src/reducers/motDePasseOublieReducer.js
+++ src/reducers/motDePasseOublieReducer.js
@@ -2,7 +2,7 @@ export default function motDePasseOublie(state = [], action) {
   switch (action.type) {
     case 'SEND_EMAIL_SUCCESS':
       return {
-        success: action.response
+        success: action.response.successResetPassword
       };
     case 'SEND_EMAIL_FAILURE':
       return {
