a0e907b87923681ccf3f0ef3622d02855d41940f
Loïc Guillois <contact@loicguillois.fr>
2021-08-11 12:03:38 +0200

    wording création compte / mot de passe oublié

--- src/components/anonymous/ChoosePassword.js
+++ src/components/anonymous/ChoosePassword.js
@@ -83,7 +83,7 @@ function ChoosePassword({ match }) {
                 {tokenVerified === false &&
                   <div className="rf-mb-10w">
                     <div className="erreur-token">
-                      <div className="invalid">Désolé mais le lien est invalide.</div>
+                      <div className="invalid">Désolé mais le lien est invalide ou a déjà été utilisé. Veuillez réinitialiser votre mot de passe si nécessaire.</div>
                     </div>
                   </div>
                 }
