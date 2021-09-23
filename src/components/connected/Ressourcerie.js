28be92d76b9ba7d8772e7b234cb58650b36fdc83
morgan <morgan@fitlab.fr>
2021-09-23 16:25:54 +0200

    titre ressourcerie

--- src/components/connected/Ressourcerie.js
+++ src/components/connected/Ressourcerie.js
@@ -3,10 +3,12 @@ import React from 'react';
 function Ressourcerie() {
 
   return (
-    <div className="rf-container">
-      <div className="rf-grid-row">
-        <div className="rf-col-12 rf-col-md-10">
-          <h1 className="titre rf-mt-2w rf-mb-1w rf-mt-md-5w rf-mb-md-8w">Ressourcerie</h1>
+    <div className="ressourcerie">
+      <div className="rf-container">
+        <div className="rf-grid-row">
+          <div className="rf-col-12 rf-col-md-10">
+            <h1 className="titre rf-mt-2w rf-mb-1w rf-mt-md-5w rf-mb-md-6w">Ressourcerie</h1>
+          </div>
         </div>
       </div>
     </div>
