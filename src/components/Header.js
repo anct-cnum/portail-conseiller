69800a5e000204d373f482d1483b01a83141f081
morgan <morgan@fitlab.fr>
2021-10-05 20:28:40 +0200

    eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Menu from './connected/Menu';
 import { menuActions } from '../actions';
-import { convertToObject } from 'typescript';
 
 function Header({ linkAccount }) {
 
