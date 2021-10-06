34553d70a118010eb28db4f47d07b77ceec1bd19
morgan <morgan@fitlab.fr>
2021-10-06 10:33:06 +0200

    Cacher la ressourcerie le temps de la classification

--- src/components/connected/Menu.js
+++ src/components/connected/Menu.js
@@ -46,6 +46,7 @@ function Menu() {
                     Enregistrer un nouvel accompagnement
                 </Link>
               </li>
+              {/*
               <li className="rf-nav__item">
                 <Link
                   className={`rf-nav__link linkCustom ${location.pathname === '/ressourcerie' ? 'linkActive' : ''}`}
@@ -53,6 +54,7 @@ function Menu() {
                     Ressourcerie
                 </Link>
               </li>
+              */}
               {!menu.hiddenMenu && menu.expandNav &&
               <li className="rf-shortcuts__item">
                 <Link className="rf-btn rf-btn--sm" to="/login" title="Se déconnecter" onClick={toggleBurgerMenu}>
