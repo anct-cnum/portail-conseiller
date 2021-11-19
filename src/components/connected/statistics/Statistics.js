acec35be14db1c5284fb4abf2a230d4195e30e85
morgan <morgan@fitlab.fr>
2021-11-19 12:48:44 +0100

    correction du problème delint

--- src/components/connected/statistics/Statistics.js
+++ src/components/connected/statistics/Statistics.js
@@ -113,8 +113,8 @@ function Statistics() {
           <h2 className="centrerTexte">Il n&rsquo;y a aucune statistique pour le moment</h2>
         }
       </div>
-      <StatisticsBanner dateDebut={dateDebutStats} dateFin={dateFinStats}
-        idTerritoire={territoire?.[typeTerritoire]} />
+      <StatisticsBanner /*dateDebut={dateDebutStats} dateFin={dateFinStats}
+        idTerritoire={territoire?.[typeTerritoire]}*/ />
       <div className="rf-m-5w rf-m-md-9w rf-m-lg-15w"></div>
       <Footer type="support"/>
     </div>
