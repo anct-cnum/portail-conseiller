69ac876f431137c66dec201056084b5548b0921e
morgan <morgan@fitlab.fr>
2022-01-03 16:01:27 +0100

    responsive et accents

--- src/components/connected/FormulaireHorairesAdresse.js
+++ src/components/connected/FormulaireHorairesAdresse.js
@@ -15,8 +15,8 @@ function FormulaireHorairesAdresse() {
       <div id="formulaire-horaires-adresse" className="rf-container">
         <div className="rf-grid-row">
           <div className="rf-col-12">
-            <h1 className="titre rf-mt-9w rf-mb-1w">Informations d’accès et contact CnFS </h1>
-            <p className="derniere-modification rf-mb-5w">Dernière modification de vos informations effectuée le {date}</p>
+            <h1 className="titre rf-mt-9w rf-mb-1w">Informations d&rsquo;acc&egrave;s et contact CnFS </h1>
+            <p className="derniere-modification rf-mb-5w">Derni&egrave;re modification de vos informations effectu&eacute;e le {date}</p>
             <h2 className="sous-titre rf-mb-4w">{structure?.nom}</h2>
             <p className="rf-mb-5w">
               <span className="libelle-adresse rf-mr-3w">Adresse</span>
@@ -42,7 +42,7 @@ function FormulaireHorairesAdresse() {
             <form className="rf-container rf-container--fluid">
               <div className="rf-grid-row rf-grid-row--gutters">
                 <p className="question rf-col-12 rf-mb-5w">
-                  Ces informations correspondent-elles à votre lieu principal d’activité ? <span className="obligatoire">*</span>
+                  Ces informations correspondent-elles à votre lieu principal d&rsquo;activit&eacute; ? <span className="obligatoire">*</span>
                   <fieldset className="rf-fieldset rf-fieldset--inline rf-mt-2w">
                     <div className="rf-fieldset__content">
                       <div className="rf-radio-group">
@@ -57,160 +57,163 @@ function FormulaireHorairesAdresse() {
                   </fieldset>
                 </p>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="lieu-activite">
-                  Nom de mon lieu principal d&rsquo;activité <span className="obligatoire">*</span>
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="lieu-activite">
+                  Nom de mon lieu principal d&rsquo;activit&eacute; <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="lieu-activite" name="lieu-activite"/>
                 </label>
 
-                <label className="rf-label rf-col-4" htmlFor="siret">
-                  Numéro de siret (optionnel)<i className="rf-ml-1w ri-information-line ri-xl ri-info"></i>
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="siret">
+                  Num&eacute;ro de siret (optionnel)
+                  <a href="https://www.pappers.fr/" title="Liens vers https://www.pappers.fr/" target="blank" rel="noreferrer">
+                    <i className="rf-ml-1w ri-information-line ri-xl ri-info"></i>
+                  </a>
                   <input className="rf-input rf-mt-2v" type="number" id="siret" name="siret"/>
                 </label>
 
                 <div className="rf-col-offset-3"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="numero-telephone">
-                  Numéro de téléphone (accueil) <span className="obligatoire">*</span>
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="numero-telephone">
+                  Num&eacute;ro de t&eacute;l&eacute;phone (accueil) <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="numero-telephone" name="numero-telephone"/>
                 </label>
 
                 <div className="rf-col-offset-4"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="email">
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="email">
                   Mail <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="email" name="email"/>
                 </label>
 
                 <div className="rf-col-offset-4"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-9w" htmlFor="site-web">
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-9w" htmlFor="site-web">
                   Site web (optionnel)
                   <input className="rf-input rf-mt-2v" type="text" id="site-web" name="site-web"/>
                 </label>
 
-                <h2 className="sous-titre rf-col-12 rf-mb-4w">Coordonnées de mon lieu d’activité</h2>
+                <h2 className="sous-titre rf-col-12 rf-mb-4w">Coordonn&eacute;es de mon lieu d&rsquo;activit&eacute;</h2>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="numero-voie">
-                  Numéro de voie <span className="obligatoire">*</span>
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="numero-voie">
+                  Num&eacute;ro de voie <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="numero-voie" name="numero-voie"/>
                 </label>
 
                 <div className="rf-col-offset-4"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="rue-voie">
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="rue-voie">
                   Rue <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="rue-voie" name="rue-voie"/>
                 </label>
 
                 <div className="rf-col-offset-4"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-5w" htmlFor="code-postal">
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-5w" htmlFor="code-postal">
                   Code Postal <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="code-postal" name="code-postal"/>
                 </label>
 
                 <div className="rf-col-offset-4"></div>
 
-                <label className="rf-label rf-col-4 rf-mb-9w" htmlFor="ville">
+                <label className="rf-label rf-col-10 rf-col-sm-7 rf-col-md-4 rf-mb-9w" htmlFor="ville">
                   Ville <span className="obligatoire">*</span>
                   <input className="rf-input rf-mt-2v" type="text" id="ville" name="ville"/>
                 </label>
 
-                <h2 className="sous-titre rf-col-12 rf-mb-4w">Horaires de mon lieu d’activité</h2>
+                <h2 className="sous-titre rf-col-12 rf-mb-4w">Horaires de mon lieu d&rsquo;activit&eacute;</h2>
                 <div className="rf-col-12">
                   <table className="rf-mb-9w">
                     <thead>
                       <tr>
                         <th></th>
-                        <th>Matin</th>
-                        <th>Après-midi</th>
+                        <th><div className="rf-mr-md-2w">Matin</div></th>
+                        <th><div className="rf-mr-md-2w">Apr&egrave;s-midi</div></th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
                         <td className="jour">Lundi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Mardi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Mercredi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Jeudi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Vendredi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Samedi</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                       <tr>
                         <td className="jour">Dimanche</td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                         <td>
-                          <input className="horaires-debut rf-mb-1w" type="time" required/>
+                          <input className="horaires-debut rf-mb-md-1w" type="time" required/>
                           <input className="horaires-fin rf-mr-2w" type="time" required/>
                         </td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
-                <h2 className="sous-titre rf-col-12 rf-mb-4w">A propos de mon activité de conseiller numérique</h2>
+                <h2 className="sous-titre rf-col-12 rf-mb-4w">A propos de mon activit&eacute; de conseiller num&eacute;rique</h2>
 
                 <p className="question rf-col-12 rf-mb-9w">
-                  Effectuez-vous des accompagnementsen itinérance ? <span className="obligatoire">*</span>
+                  Effectuez-vous des accompagnements en itin&eacute;rance ? <span className="obligatoire">*</span>
                   <fieldset className="rf-fieldset rf-fieldset--inline rf-mt-2w">
                     <div className="rf-fieldset__content">
                       <div className="rf-radio-group">
