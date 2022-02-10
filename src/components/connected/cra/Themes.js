10e1e655446a1a3272c0e25ee3fba94b9e95e76b
morgan <morgan@fitlab.fr>
2022-02-10 16:59:16 +0100

    Retrait du composant

--- src/components/connected/cra/Themes.js
+++ src/components/connected/cra/Themes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import BigCheckboxButton from './Components/BigCheckboxButton';
-import SmallCheckboxButton from './Components/SmallCheckboxButton';
 
 function Themes() {
 
