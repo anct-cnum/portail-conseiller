09fb67bf99cab6aa43f2fa52fe97174593e5db05
Sébastien Quirrion <sebastien@fitlab.fr>
2022-02-22 18:58:41 +0100

    Migration du sort cote API

--- src/utils/functionsSort.js
+++ src/utils/functionsSort.js
@@ -1,17 +1,3 @@
-import labelsCorrespondance from '../data/labelsCorrespondance.json';
-
-export function sortByValueThenName(a, b) {
-  if (a.valeur > b.valeur) {
-    return -1;
-  }
-  if (a.valeur < b.valeur) {
-    return 1;
-  }
-  const libelle1 = labelsCorrespondance.find(label => label.nom === a.nom)?.correspondance ?? a.nom;
-  const libelle2 = labelsCorrespondance.find(label => label.nom === b.nom)?.correspondance ?? b.nom;
-  return libelle1.localeCompare(libelle2, 'fr');
-}
-
 export function sortByMonthAndYear(a, b) {
   if (a.annee === b.annee) {
     if (a.mois < b.mois) {
