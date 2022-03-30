26e15ff693600a70cce3065cfd151fb77a666459
Ornella Ourfi <ornella93@outlook.fr>
2022-03-30 11:22:54 +0200

    question optionnel donc on enlève le rouge de required

--- src/components/connected/cra/Recurrence.js
+++ src/components/connected/cra/Recurrence.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import RecurrenceButton from './Components/RecurrenceButton';
 
 function Recurrence() {
 
-  let cra = useSelector(state => state.cra);
-
   return (
     <div className="rf-grid-row rf-grid-row--gutters rf-grid-row--middle espacement">
       <div className="rf-col-xs-11 rf-col-sm-11 rf-col-md-2 questionResponsive">
-        <span className={`question ${cra?.printError && cra?.errorsRequired?.duree ? 'questionRequired' : ''}`}>
-          Optionnel&nbsp;: r&eacute;currence des personnes
-        </span>
+        <span className="question">Optionnel&nbsp;: r&eacute;currence des personnes</span>
       </div>
       <div className="responsiveRecurrence">
         <RecurrenceButton />
