de11f15bab3beca25a45b45fcf60322c6553af19
Ornella Ourfi <ornella93@outlook.fr>
2022-05-09 18:45:30 +0200

    retour css

--- src/components/connected/Welcome.js
+++ src/components/connected/Welcome.js
@@ -30,7 +30,7 @@ function Welcome() {
                 <div className="rf-grid-row rf-grid-row--center">
                   <div className="rf-col-12 rf-mt-12w rf-mb-3w">
                     { !conseiller?.supHierarchique &&
-                      <div className="div-info-rappel-hierarchique">
+                      <div className="div-info-rappel-hierarchique rf-mb-3w rf-py-1w">
                         <i className="ri-information-line information icone-info-rappel"></i>
                         <p>
                           Afin que votre responsable hi&eacute;rarchique puisse recevoir toutes les informations concernant le dispositif et vous accompagner
