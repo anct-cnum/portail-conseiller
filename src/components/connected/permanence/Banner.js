2998369a2af559283af8f63ad236594bdcaf0d15
morgan <morgan@fitlab.fr>
2022-05-25 16:58:37 +0200

    changement wording banière

--- src/components/connected/permanence/Banner.js
+++ src/components/connected/permanence/Banner.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Banner() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="banniere">
       <div className="rf-container">
@@ -15,9 +18,19 @@ function Banner() {
           </div>
           <div className="rf-col-11 rf-col-md-6 texte-banniere rf-mb-7w">
             <span className="important violet">Dans cette page, vous renseignez :</span><br/><br/>
-            Vos <span className="important blanc">lieux d&rsquo;activit&eacute;</span>,
-            ceux-ci seront visibles sur la carte nationale des conseillers num&eacute;riques.<br/><br/>
-            Les <span className="important vert">informations professionnelles</span> qui serviront &agrave;
+            {location.pathname === 'mes-lieux-activite' &&
+            <>
+              <span className="important blanc">Vos lieux d&rsquo;activit&eacute;</span>,
+              ceux-ci seront visibles sur la carte nationale des conseillers num&eacute;riques.
+            </>
+            }
+            {location.pathname !== 'mes-lieux-activite' &&
+              <><span className="important blanc">Votre lieu d&rsquo;activit&eacute;</span>,
+              celui-ci sera visible sur la carte nationale des conseillers num&eacute;riques.
+              </>
+            }
+            <br/><br/>
+             Les <span className="important vert">informations professionnelles</span> qui serviront &agrave;
             vous donner de la visibilit&eacute; aupr&egrave;s de vos publics,
              celles-ci seront visibles sur la carte.
           </div>
