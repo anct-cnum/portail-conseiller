e4f7681e9133fbb7549eacbbb7cdec3c0c0a5997
morgan <morgan@fitlab.fr>
2022-11-17 12:38:20 +0100

    Ajout amélioration en fonction des retours

--- src/components/connected/statistics/Components/ElementCodePostal.js
+++ src/components/connected/statistics/Components/ElementCodePostal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { statistiqueActions } from '../../../../actions';
 import PropTypes from 'prop-types';
@@ -8,10 +8,12 @@ function ElementCodePostal({ idStructure = '' }) {
   const dispatch = useDispatch();
   const listeCodesPostaux = useSelector(state => state.statistique?.listeCodesPostaux);
   const setCodePostal = e => {
-    const codePostal = e.target.value.split(' - ')[0];
-    const ville = e.target.value.split(' - ')[1];
+    const codePostal = e.target.value.split('-')[0];
+    const ville = e.target.value.split('-')[1];
     dispatch(statistiqueActions.changeCodePostalStats(codePostal, ville));
   };
+
+  const [optionList, setOptionList] = useState([]);
 
   useEffect(() => {
     if (!listeCodesPostaux) {
@@ -20,6 +22,28 @@ function ElementCodePostal({ idStructure = '' }) {
       } else {
         dispatch(statistiqueActions.getCodesPostauxCrasConseiller());
       }
+    } else if (listeCodesPostaux && optionList.length === 0) {
+      listeCodesPostaux.forEach(codePostal => {
+        if (codePostal.villes.length === 1) {
+          optionList.push({
+            text: codePostal.id + ' - ' + codePostal.villes[0].toUpperCase(),
+            value: codePostal.id + '-' + codePostal.villes[0]
+          });
+        } else if (codePostal.villes.length > 1) {
+          optionList.push({
+            text: codePostal.id + ' - TOUTES COMMUNES',
+            value: codePostal.id
+          });
+          codePostal.villes.forEach(ville => {
+            optionList.push({
+              text: ville,
+              value: codePostal.id + '-' + ville,
+              marge: '- - '
+            });
+          });
+        }
+      });
+      setOptionList(optionList);
     }
   });
 
@@ -29,16 +53,14 @@ function ElementCodePostal({ idStructure = '' }) {
       {idStructure.length > 0 && listeCodesPostaux && listeCodesPostaux?.map((codePostal, idx) => {
         return (<option key={idx} value={codePostal}>{codePostal}</option>);
       })}
-      {idStructure.length === 0 && listeCodesPostaux && listeCodesPostaux?.map((codePostal, idx) => {
-        return (<optgroup key={idx} label={codePostal.id}>
-          {codePostal?.codePostal?.length > 1 &&
-            <option value={codePostal.id}>{codePostal.id} - TOUTES COMMUNES </option>
-          }
-          {codePostal?.codePostal?.map((ligne, idxbis) => {
-            return (<option key={idxbis} value={ligne}>{ligne.toUpperCase()}</option>);
-          })}
-        </optgroup>);
-      })}
+      {idStructure.length === 0 && listeCodesPostaux && optionList?.map((option, idx) => {
+        return (
+          <option key={idx} value={option.value}>
+            {option?.marge}{option.text}
+          </option>
+        );
+      })
+      }
     </select>
   );
 }
