91635030494b8ed7ad5b530d2033abb51eca2c5a
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2024-03-08 17:40:58 +0100

    fix: route pour les coordinateurs

--- src/components/coordinateur/index.js
+++ src/components/coordinateur/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Navigate } from 'react-router-dom';
+import { Route, Navigate, Routes } from 'react-router-dom';
 import Header from '../Header';
 import CoordinateurHeader from './CoordinateurHeader';
 import Conseillers from './Conseillers';
@@ -52,35 +52,37 @@ function Coordinateur() {
       <Header linkAccount={user?.name} />
       <div className="admin">
         <CoordinateurHeader linkAccount={user?.name} />
-        {(!voirPermanence || suspendrePermanence) &&
-          <>
-            <Route path={'/mes-informations'} element={<MesInformations />} />
-            <Route exact path={`/compte-rendu-activite`} element={<Cra />} />
-            <Route path={`/compte-rendu-activite/:idCra`} element={<UpdateCra />} />
-            <Route path={'/mes-lieux-activite'} element={<MesPermanences />} />
-            <Route path={'/mon-lieu-activite/:idPermanence'} element={<PermanenceUpdate />} />
-            <Route path={'/mon-nouveau-lieu-activite'} element={<PermanenceCreate />} />
-            <Route path={`/accueil`} element={<Conseillers />} />
-            <Route path={`/territoires`} element={<Territoires />} />
-            <Route path={`/statistiques`} element={<Statistics />} />
-            <Route path={`/conseiller/:id`} element={<ConseillerDetails />} />
-            <Route path={`/lieux-activite`} element={<Permanence />} />
-            <Route path={'/historique'} element={<HistoriqueCras />} />
-            <Route path={'/mon-espace-candidat'} element={<MonEspaceCandidat />} />
-            <Route path={'/contact-mon-responsable'} element={<FormulaireSuperieurHierarchique />} />
-            <Route exact path="/" element={<Navigate to="/accueil" />} />
-          </>
-        }
-        {(voirPermanence && !suspendrePermanence) &&
-          <>
-            <Route path={`/accueil`} element={<Permanence />} />
-            <Route path="/" element={<Navigate to="/accueil" />} />
-          </>
-        }
+        <Routes>
+          {(!voirPermanence || suspendrePermanence) &&
+            <>
+              <Route path={'/mes-informations'} element={<MesInformations />} />
+              <Route exact path={`/compte-rendu-activite`} element={<Cra />} />
+              <Route path={`/compte-rendu-activite/:idCra`} element={<UpdateCra />} />
+              <Route path={'/mes-lieux-activite'} element={<MesPermanences />} />
+              <Route path={'/mon-lieu-activite/:idPermanence'} element={<PermanenceUpdate />} />
+              <Route path={'/mon-nouveau-lieu-activite'} element={<PermanenceCreate />} />
+              <Route path={`/accueil`} element={<Conseillers />} />
+              <Route path={`/territoires`} element={<Territoires />} />
+              <Route path={`/statistiques`} element={<Statistics />} />
+              <Route path={`/conseiller/:id`} element={<ConseillerDetails />} />
+              <Route path={`/lieux-activite`} element={<Permanence />} />
+              <Route path={'/historique'} element={<HistoriqueCras />} />
+              <Route path={'/mon-espace-candidat'} element={<MonEspaceCandidat />} />
+              <Route path={'/contact-mon-responsable'} element={<FormulaireSuperieurHierarchique />} />
+              <Route exact path="/" element={<Navigate to="/accueil" />} />
+            </>
+          }
+          {(voirPermanence && !suspendrePermanence) &&
+            <>
+              <Route path={`/accueil`} element={<Permanence />} />
+              <Route path="/" element={<Navigate to="/accueil" />} />
+            </>
+          }
 
-        {voirFormulaire &&
-          <FormulaireSexeAge />
-        }
+          {voirFormulaire &&
+            <FormulaireSexeAge />
+          }
+        </Routes>
       </div>
     </>
   );
